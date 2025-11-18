1f0bcffe638fa088298d4d36955ba62cf37f4edd
sameerraza7760 <sameerraza7760@gmail.com>
2025-11-18 13:30:26 +0500

    fix: correct property name for user's message acceptance status

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -90,7 +90,7 @@ export async function GET(request: Request) {
     return Response.json(
       {
         success: true,
-        isAcceptingMessages: foundUser.isAcceptingMessage,
+        isAcceptingMessages: foundUser.isAcceptingMessages,
       },
       { status: 200 }
     );
